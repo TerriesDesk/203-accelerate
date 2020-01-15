1652b75fed78b9e5890e49875c748a7b5fa2048e
Terrie's Desk <tsw659@gmail.com>
2020-01-15 17:01:34 -0500

    more acf

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -13,17 +13,41 @@
  */
 
 get_header(); ?>
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post();
-      $services 
+<div id="primary" class="site-content sidebar">
+	<div class="main-content" role="main">
+		<?php while ( have_posts() ) : the_post();
+      $services = get_field('services');
+      $client = get_field('client');
+      $link = get_field('site_link');
+      $image1 = get_field('image_1');
+      $image2 = get_field('image_2');
+      $image3 = get_field('image_3');
+      $size = "full";
+    ?>
+   <article class="case-study">
+     <aside class="case-study-sidebar">
+       <h2><?php the_title(); ?></h2>
+       <h4><span><?php echo $services; ?></span></h4>
+       <h4><strong><?php echo $client; ?></strong></h4>
+       <?php the_content(); ?>
+       <p class="read-more-link"><a href="<?php echo $link; ?>">Visit Live Site</a></p>
+    </aside>
+    <div class="case-study-images">
+      <?php if($image1) {
+        echo wp_get_attachment_image($image1, $size);
+      } ?>
 
+      <?php if($image2) {
+        echo wp_get_attachment_image($image2, $size);
+      } ?>
 
-       ?>
-        <?php the_title(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
+      <?php if($image3) {
+        echo wp_get_attachment_image($image3, $size);
+      } ?>
+    </div>
+  </article>
+  <?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
 
 	</div><!-- #primary -->
 <?php get_footer(); ?>
