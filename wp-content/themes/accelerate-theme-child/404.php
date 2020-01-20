80bf7c8fa3ca7d5d336fec64b49cab9575c80c02
Terrie's Desk <tsw659@gmail.com>
2020-01-19 22:54:05 -0500

    Final Bells and Whistles

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -13,19 +13,14 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-			<?php endwhile; // end of the loop. ?>
-      <img src="twitter.png" />
-      <div class="error-page-text">
-          <h2> Whoops, Took a Wrong Turn...</h2>
-          <p>Sorry, this page no longer exists, never existed, or has been moved.  We feel like complete jerks for totally misleading you.</p>
-          <p>Feel free to take a look around our <a href="#"><span>blog<span></strong></a> or some of our featured <a href="#"><span>work</span></a>.</p>
-        </div>
-
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
+ <div class="non-hero-image">
+  <div class="main-content" role="main">
+  <div class="error-page-text">
+    <h2> Whoops, Took a Wrong Turn...</h2>
+    <h6>Sorry, this page no longer exists, never existed, or has been moved.  We feel like complete jerks for totally misleading you.</h6>
+    <h6>Feel free to take a look around our <a href="#"><span>blog<span></strong></a> or some of our featured <a href="#"><span>work</span></a>.</h6>
+  </div>
+</div><!-- .main-content -->
+</div>
 
 <?php get_footer(); ?>
